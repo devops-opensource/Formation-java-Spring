e3af96531265c767d08029419ee153d6e788196b
lkdGologic <laurence-karl.davidson@gologic.ca>
2023-03-10 15:50:28 -0500

    GOFOR-15 | Lab-2 creation d'un service contenant un Logger.

--- src/main/java/com/formation/lab/LabApplication.java
+++ src/main/java/com/formation/lab/LabApplication.java
@@ -1,13 +1,17 @@
 package com.formation.lab;
 
+import com.formation.lab.service.SimpleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(LabApplication.class, args);
+        SimpleService simpleService = ctx.getBean(SimpleService.class);
+        simpleService.functionLog();
+    }
 
 }
